b5f040be223fa09692c0e08807bc0d587276d846
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-09-12 20:36:11 +0600

    still

--- csv-app/index.js
+++ csv-app/index.js
@@ -1,11 +1,47 @@
-import { writeFileSync } from "fs"
+import { appendFileSync } from "fs"
+import { createInterface } from "readline"
+import { promisify } from "util"
 
-const content = "Test content!!"
 
-try {
-  writeFileSync("./test.txt", content)
-  console.log("Success!")
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-}catch (err){
-  console.error(err)
+const readLineAsync = promisify(readline.question).bind(rl)
+(async () => {
+  try {
+    const name = await readLineAsync('what is your name ?')
+    console.log(`Hello ${name}`)
+  } catch (err) {
+    console.error('Error:', err.message)
+  }finally {
+    readline.close()
+  }
+})
+
+class Person {
+  constructor(name = "", number = "", email = ""){
+    this.name = name
+    this.number = number
+    this.email = email
+  }
+  saveToCSV(){
+    const content = `${this.name}, ${this.number}, ${this.email}\n`
+    try {
+      appendFileSync("./contacts.csv", content)
+      console.log(`${this.name} Saved!`)
+    } catch(err){
+      console.error(err)
+    }
+
+  }
+}
+
+const startApp = async () => {
+  let shouldContinue = true
+  while ( shouldContinue ) {
+    const name = await readLineAsync("Contact Name: ")
+
+  }
 }
